4f8e1e774eba9d60bc094159864b6384b373e016
machego <114348981+machego@users.noreply.github.com>
2023-04-02 17:45:43 +0200

    first version

---
+++ client/src/App.js
@@ -0,0 +1,60 @@
+import React from "react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
+import Home from "./pages/home/Home";
+import Hotel from "./pages/hotel/Hotel";
+import List from "./pages/list/List";
+import Navbar from "./components/navbar/Navbar"
+import Footer from "./components/footer/Footer"
+
+
+
+
+
+function App() {
+  /*
+  const Layout = () => {
+    return (
+      <div className="app">
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
+    );
+  };
+  */
+
+  const router = createBrowserRouter([
+    /*{
+      path: "/",
+      element: <Layout />,
+      children: [*/
+        {
+          path: "/",
+          element: <Home/>
+        },
+        {
+          path: "/hotels",
+          element: <List/>
+        },
+        {
+          path: "/hotels/:id",
+          element: <Hotel/>
+        }
+      ]
+   /* },
+  ]*/);
+
+
+  return (
+    <div>
+      <RouterProvider router={router} />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,28 @@
+import './home.css'
+import Navbar from '../../components/navbar/Navbar'
+import Header from '../../components/header/Header'
+import Footer from '../../components/footer/Footer';
+import Featured from '../../components/featured/Featured';
+import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties';
+import MailList from '../../components/mailList/MailList';
+import PropertyList from '../../components/propertyList/PropertyList';
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Header/>
+      <div className="homeContainer">
+        <Featured/>
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList/>
+        <h1 className="homeTitle">Homes guests love</h1>
+        <FeaturedProperties/>
+        <MailList/>
+        <Footer/>
+      </div>
+    </div>
+  );
+};
+
+export default Home
