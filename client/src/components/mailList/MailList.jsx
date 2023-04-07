709c483bbf1e664a139300e5bed5dd17d41d4bb9
machego <114348981+machego@users.noreply.github.com>
2023-04-07 18:23:31 +0200

    update

--- client/src/components/mailList/MailList.jsx
+++ client/src/components/mailList/MailList.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import './mailList.css'
+import "./mailList.css"
 
 const MailList = () => {
   return (
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
-import './Navbar.css'
-
+import "./navbar.css";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
 const Navbar = () => {
-  return (
-    <div className='navbar'>
-      <div className='navContainer'>
-        <span className='logo'>ibooking</span> 
-        <div className='navItems'>
-          <button className='navButton'>Register</button>
-          <button className='navButton'>Login</button> 
-        </div>
-      </div> 
-    </div>
-  )
-}
+  const { user } = useContext(AuthContext);
 
-export default Navbar
+  return (
+    <div className="navbar">
+      <div className="navContainer">
+        <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+          <span className="logo">ibooking</span>
+        </Link>
+        {user ? user.username : (
+          <div className="navItems">
+            <button className="navButton">Register</button>
+            <button className="navButton">Login</button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
